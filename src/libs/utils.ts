4340eaaeccf3ee5f5e5065c5450bba0e17c814e4
fujitacode <riot.jack.bbb@gmail.com>
2026-07-13 23:23:50 +0900

    #1 開発体験が悪すぎたので開発環境を再編

--- src/libs/utils.ts
+++
@@ -1,4 +0,0 @@
-//余計な余白を消す
-export const cn = (...args: (string | undefined | null | false)[]) => {
-	return args.filter(Boolean).join(" ");
-};
--- src/pages/CatalogPage.ts
+++
@@ -1,5 +0,0 @@
-export const CatalogPage = () => `
-  <h1>コンポーネント一覧</h1>
-  <p>ここに100個のパーツが並びます</p>
-  <button data-link="/">ホームに戻る</button>
-`;
